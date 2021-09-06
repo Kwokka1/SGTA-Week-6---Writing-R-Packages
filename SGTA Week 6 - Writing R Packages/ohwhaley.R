2f17ab03cc71da126007dc969846fa8ce686db69
Ethan Kwok <ethan.kwok@students.mq.edu.au>
2021-09-06 10:50:52 +1000

    Initial commit

---
+++ SGTA Week 6 - Writing R Packages/ohwhaley.R
@@ -0,0 +1,15 @@
+usethis::use_r("ohwhaley")
+devtools::load_all()
+devtools::check()
+usethis::use_mit_license("Jane Doe")
+
+devtools::document()
+?ohwhaley
+usethis::use_package("glue")
+usethis::use_pipe()
+
+usethis::use_testthat()
+usethis::use_test("ohwhaley")
+library(testthat)
+
+say("uwu")
